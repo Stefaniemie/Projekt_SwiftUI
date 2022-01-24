d7a8938860b32469cf6413106723a3903a497736
Stefanie Miebach / BBS2H20A <Stefanie.Miebach@edu.bib.de>
2022-01-24 08:57:46 +0100

    Projekt angelegt

---
+++ SwiftUI.Projekt/SwiftUI.Projekt/SwiftUI_ProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjektApp.swift
+//  SwiftUI.Projekt
+//
+//  Created by Stefanie Miebach / BBS2H20A on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
