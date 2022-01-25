db6854eb2eb3f2518fc142556e3e3cd1c1898280
Stefanie Miebach / BBS2H20A <Stefanie.Miebach@edu.bib.de>
2022-01-25 08:35:20 +0100

    Images eingefügt.

--- SwiftUI.Projekt/SwiftUI.Projekt/ContentView.swift
+++ SwiftUI.Projekt/SwiftUI.Projekt/ContentView.swift
@@ -14,29 +14,67 @@ struct ContentView: View {
         HStack {
             Button(action: {
                 print("Winter")
+                    
                 })
             {
                 Image("Image-1")
+                    .frame(width: 200, alignment: .leading)
                         
             }
+            
+            HStack {
+                Button(action: {
+                    print("Summer")
+                    })
+                {
+                    Image("sun_2600-fe0f")
+                        .frame(width: 200, alignment: .leading)
+                            
+                }
+                
+            }
+            
+            HStack {
+                Button(action: {
+                    print("herbst")
+                    })
+                {
+                    Image("herbst")
+                        .frame(width: 200, alignment: .leading)
+                                                    
+                }
                 
             
-        }
-    }
-
+            }
+            
+            HStack {
+                Button(action: {
+                    print("Fruehling")
+                    })
+                {
+                    Image("fruehling")
+                        .frame(width: 2, alignment: .leading)
+                    
+                            
+                }
+                
+            
+    
+            }
    
-
-
-
-
+        }
+        
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         
         
-        
-        
+
         
     }
+   }
   }
-}
+
+
+
